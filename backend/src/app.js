a7221c4605059e57c717f7221505ce4ff9b84891
bhargavnama <bhargavnama143@gmail.com>
2025-03-18 00:06:55 +0530

    Added .env file

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,15 +2,17 @@ import express from 'express';
 import { createServer } from 'node:http';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 import connectToSocket from './controllers/socketManager.js';
 import userRoutes from './routes/user.routes.js';
 
+dotenv.config();
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
+const PORT = process.env.PORT || 8000;
 
-app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
@@ -18,10 +20,10 @@ app.use(express.urlencoded({limit: "40kb", extended: true}));
 app.use('/api/v1/users', userRoutes);
 
 const start = async () => {
-    const connectionDB = await mongoose.connect('mongodb+srv://bhargavnama143:sUcripLhCnV1WAcl@cluster0.1a89u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    const connectionDB = await mongoose.connect(process.env.MOGO_URI);
     console.log(`MonogoDB Connected host: ${connectionDB.connection.host}`);
-    server.listen(app.get('port'), () => {
-        console.log('Server is running on port 8000');
+    server.listen(PORT, () => {
+        console.log('Server is running on port ', PORT);
     })
 }
 
